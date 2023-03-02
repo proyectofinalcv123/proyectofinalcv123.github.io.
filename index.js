c9612abdb97b413f9ac9b8cf2dd082979e9448d0
proyectofinalcv123 <126738300+proyectofinalcv123@users.noreply.github.com>
2023-03-01 23:14:15 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('gmail').addEventListener('click',function () {
+ document.getElementById("info").innerHTML="Mi gmail es hazelmendoza@example.com " 
+});
+
+document.getElementById('direc').addEventListener('click',function () {
+document.getElementById("info").innerHTML="Mi dirección es 1509 Valwood Pkwy " 
+});
+
+document.getElementById('cumple').addEventListener('click',function () {
+document.getElementById("info").innerHTML="Mi fecha de nacimiento es 9/5/1974 " 
+});
+   
+document.getElementById('tel').addEventListener('click',function () {
+document.getElementById("info").innerHTML="Mi número telefonico es (770) 540-0428 " 
+});
